222d69b58798cd9b9c451889c37561c65e7adbb7
meints <cadumeints0@gmail.com>
2026-05-26 23:25:21 -0300

    feat: adicionar verificação de domínio do Facebook e integrar Meta Pixel ao layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -59,6 +59,30 @@ export default function RootLayout({
       <head>
         {/* Facebook Domain Verification */}
         <meta name="facebook-domain-verification" content="6pi234y3nvdo79hmsty5wnbjofuf7p" />
+        {/* Meta Pixel */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1337041978352891');
+fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1337041978352891&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
         {/* Google Analytics */}
         <Script
           src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING_ID}`}
